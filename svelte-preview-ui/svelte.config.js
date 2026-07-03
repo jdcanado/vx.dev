2a25a85f12d7c7fa3138c70b5aa794fe5a7dd297
jdc <jdcanado@gmail.com>
2026-07-03 01:35:34 -0300

    Inclusao de preview no github pages

--- svelte-preview-ui/svelte.config.js
+++ svelte-preview-ui/svelte.config.js
@@ -4,14 +4,14 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
       pages: 'dist',
       assets: 'dist',
       fallback: 'index.html'
     }),
+    paths: {
+      base: process.env.BASE_URL || "",
+    },
     alias: {
       $lib: "./src/lib"
     }
